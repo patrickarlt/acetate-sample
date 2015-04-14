3bbd237cd91f0394a7c70792348424ce5893f859
Patrick Arlt <patrick.arlt@gmail.com>
2015-04-13 22:35:56 -0700

    updates

--- acetate.conf.js
+++ acetate.conf.js
@@ -1,10 +1,6 @@
 var _ = require('lodash');
-var minimatch = require('minimatch');
 
 module.exports = function (acetate) {
-
-  // set the log level we want
-  acetate.logLevel = 'debug';
 
   // apply metadata in bulk to pages mathcing **/*
   acetate.metadata('**/*', {
@@ -12,33 +8,25 @@ module.exports = function (acetate) {
     author: 'Patrick Arlt'
   });
 
-  // add a new data (data.json) file to pages with the variable `merge`
-  acetate.metadata('data.html', {
-    data: {
-      merge: 'data.json'
-    }
-  });
+  acetate.data('global', 'global.json');
 
   // all pages matching **/* will extend the 'content' block inside _layout.html
   acetate.layout('**/*', '_layout:content');
   acetate.layout('blog/posts/**/*', 'blog/_layout:post');
 
-  acetate.collection('posts', 'blog/posts/*', {
-    sortBy: 'date',
-    ascending: false
-  });
-
-  acetate.registerHelper('helper', function(foo, bar, baz){
+  acetate.helper('helper', function(foo, bar, baz){
     return 'Custom helper called with ' + foo + ' ' + bar + ' '  + baz;
   });
 
-  acetate.registerBlock('content', function(body, foo, bar, baz){
+  acetate.block('content', function(body, foo, bar, baz){
     return 'Block helper called with ' + foo + ' ' + bar + ' '  + baz + ' \n' + body;
   });
 
-  acetate.query('blog', function(pages){
-    return _(pages).filter(function(page){
-      return minimatch(page.src, 'blog/posts/**/*');
-    }).sortByOrder(['date'], [false]);
+  acetate.global('log', function(value){
+    console.log(value);
+  });
+
+  acetate.query('blog', 'blog/posts/*.md', function(pages){
+    return _.sortByOrder(pages, ['date'], [false]);
   });
 };
--- src/gists.js
+++ src/gists.js
@@ -1,14 +1,14 @@
 var request = require('request');
 
-module.exports = function(callback, page){
+module.exports = function(callback){
   request({
     method: 'GET',
-    url: 'https://api.github.com/users/' + page.username + '/gists',
+    url: 'https://api.github.com/users/patrickarlt/gists',
     json: true,
     headers: {
       'User-Agent': 'request'
     }
   }, function(err, resp, body){
-    callback(err, body);
+    callback(err, body.length);
   });
 };
